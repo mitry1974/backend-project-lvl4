41d836fb3e812476f7a42d8cb773588589056bf7
DT <tomitry@gmail.com>
2020-07-14 17:18:55 +0300

    routes error handling moved to main error handler

--- server/lib/errorHandler.js
+++ server/lib/errorHandler.js
@@ -1,17 +1,32 @@
 import Rollbar from 'rollbar';
+import AuthorizationError from '../errors/AuthorizationError';
+import ValidationError from '../errors/ValidationError';
+import AutheticationError from '../errors/AutheticationError';
+import NotFoundError from '../errors/NotFoundError';
 
 export default (app) => {
+  const rollbar = new Rollbar(
+    {
+      accessToken: app.config.get('ROLLBAR_KEY'),
+      captureUncaught: true,
+      captureUnhandledRejections: true,
+    },
+  );
   app.setErrorHandler(async (error, request, reply) => {
     console.log('MAIN ERROR HANDLER');
-    const rollbar = new Rollbar(
-      {
-        accessToken: app.config.get('ROLLBAR_KEY'),
-        captureUncaught: true,
-        captureUnhandledRejections: true,
-      },
-    );
     rollbar.error(error, request);
-    reply.status(500);
-    reply.send();
+
+    const type = error.constructor.name;
+    request.log.info(`Routes error handler, error class: ${error.constructor.name}`);
+    if ((error instanceof AutheticationError)
+      || (error instanceof AuthorizationError)
+      || (error instanceof ValidationError)
+      || (error instanceof NotFoundError)) {
+      error.proceed(request, reply);
+    } else {
+      console.log(`Routes error happens: type: ${type}, error: ${JSON.stringify(error, null, '\t')}`);
+      reply.status(500);
+      reply.send();
+    }
   });
 };
