cfdde43fa8b6f98305164bc4fe0f34593cc8381f
DT <tomitry@gmail.com>
2020-10-08 05:17:29 +0300

    add seeds for tags

---
+++ server/db/seeders/20201008021223-tags.js
@@ -0,0 +1,23 @@
+const Models = require('../models');
+
+module.exports = {
+  up: async () => {
+    const values = [
+      { name: 'node.js' },
+      { name: 'php' },
+      { name: 'c++' },
+      { name: 'python' },
+    ]
+      .map(async (el) => {
+        const ts = await Models.Tag.findOne({ where: { name: el.name } });
+        if (!ts) {
+          await Models.Tag.create(el);
+        }
+      });
+
+    return Promise.all(values);
+  },
+
+  down: async () => {
+  },
+};
