3064f686f12ba9d23a40717ba17f388c46d7da73
DT <tomitry@gmail.com>
2020-10-28 18:24:07 +0300

    moved models initialization to functions from module body. added sequelize instance to load models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,30 +2,33 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const config = require('../../../dbconfig');
 
-const db = {};
-
-const models = ['User.js', 'Task.js', 'Guest.js', 'TaskStatus.js', 'Tag.js', 'TaskTags.js'];
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-models.forEach((file) => {
-  // eslint-disable-next-line
-  const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-  db[model.name] = model;
-});
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+const createSequelize = () => {
+  let sequelize;
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
   }
-});
+  return sequelize;
+};
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const initializeModels = (sequelize) => {
+  const instance = sequelize || createSequelize();
+  const models = {};
+  const modelFiles = ['User.js', 'Task.js', 'Guest.js', 'TaskStatus.js', 'Tag.js', 'TaskTags.js'];
+  modelFiles.forEach((file) => {
+    // eslint-disable-next-line
+    const model = require(path.join(__dirname, file))(instance, Sequelize.DataTypes);
+    models[model.name] = model;
+  });
 
-module.exports = db;
+  Object.keys(models).forEach((modelName) => {
+    if (models[modelName].associate) {
+      models[modelName].associate(models);
+    }
+  });
+
+  return models;
+};
+
+module.exports = initializeModels;
