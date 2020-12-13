17a5d5446a73b1783f9738ace118fccac80a4e90
DT <tomitry@gmail.com>
2020-12-13 16:53:03 +0300

    remove timestamps from taskTags table creation

--- server/db/migrations/20200831150327-create-tasks-tags.js
+++ server/db/migrations/20200831150327-create-tasks-tags.js
@@ -17,14 +17,14 @@ module.exports = {
           key: 'id',
         },
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
+      // createdAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE,
+      // },
+      // updatedAt: {
+      //   allowNull: false,
+      //   type: Sequelize.DATE,
+      // },
     });
   },
   down: async (queryInterface) => {
